31937623f2cbc11119030767fa7551a8f7e2fe84
VSanotskyi <110067026+VSanotskyi@users.noreply.github.com>
2022-12-20 16:24:08 +0000

    Deploying to gh-pages from @ VSanotskyi/goit-js-hw-08@324784f170fe7557de90f86d0f5bb42bdf6813bc 🚀

---
+++ 02-video.1c2846d6.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("iframe");new Vimeo.Player(e).on("play",(function(e){console.log(e.timeupdate)}));
+//# sourceMappingURL=02-video.1c2846d6.js.map
---
+++ 02-video.5128c933.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("iframe");new Vimeo.Player(e).on("play",(function(e){console.log(e.timeupdate)}))}();
+//# sourceMappingURL=02-video.5128c933.js.map
