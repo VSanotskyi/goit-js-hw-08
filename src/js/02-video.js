324784f170fe7557de90f86d0f5bb42bdf6813bc
VSanotskyi <110067026+VSanotskyi@users.noreply.github.com>
2022-12-20 18:21:06 +0200

    add file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,8 @@
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+player.on('play', onPlay);
+
+function onPlay(data) {
+  console.log(data.timeupdate);
+}
