fe787cc56963740a5bbadeb588e66467e2778e0d
VSanotskyi <110067026+VSanotskyi@users.noreply.github.com>
2022-12-20 21:26:07 +0200

    add file

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,8 +1,1 @@
-const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
-
-player.on('play', onPlay);
-
-function onPlay(data) {
-  console.log(data.timeupdate);
-}
+var throttle = require('lodash.throttle');
